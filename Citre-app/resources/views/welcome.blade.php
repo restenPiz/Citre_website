6da4c4bbbb63d3533f4ba1b457628d59ec3eb5f4
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-28 10:46:07 +0200

    Some updates in welcomePage

--- Citre-app/resources/views/welcome.blade.php
+++ Citre-app/resources/views/welcome.blade.php
@@ -325,10 +325,8 @@
             <div class="container" data-aos="fade-up">
 
                 <div class="section-title">
-                    <h2>Services</h2>
-                    <h3>Check our <span>Services</span></h3>
-                    <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas
-                        atque vitae autem.</p>
+                    <h2>Serviços</h2>
+                    <h3>Veja os nossos <span>Serviços</span></h3>
                 </div>
 
                 <div class="row">
