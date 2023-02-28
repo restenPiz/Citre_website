6b6876c7548a9f12514592bcaffe237a78082c8c
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-28 09:13:44 +0200

    Some updates in icons

--- Citre-app/resources/views/welcome.blade.php
+++ Citre-app/resources/views/welcome.blade.php
@@ -35,14 +35,11 @@
   <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">citre@gmail.com</a></i>
-        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">citre24@gmail.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+258 872612657 / +258 840320993</span></i>
       </div>
       <div class="social-links d-none d-md-flex align-items-center">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+        <a href="https://web.facebook.com/profile.php?id=100076387305813" class="facebook"><i class="bi bi-facebook"></i></a>
       </div>
     </div>
   </section>
