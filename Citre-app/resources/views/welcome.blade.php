05644fc7d46ea4116fc6f4d9968ed4c045dd3731
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-02-28 09:56:51 +0200

    Some updates in about section

--- Citre-app/resources/views/welcome.blade.php
+++ Citre-app/resources/views/welcome.blade.php
@@ -133,9 +133,9 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
-          <h2>About</h2>
-          <h3>Find Out More <span>About Us</span></h3>
-          <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
+          <h2>Sobre</h2>
+          <h3>Saiba mais <span>sobre nós</span></h3>
+          <!--<p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>--->
         </div>
 
         <div class="row">
