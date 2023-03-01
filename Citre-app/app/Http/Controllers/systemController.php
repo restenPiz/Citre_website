954b30cb7a4b77e59299db849216a7c63c9455f9
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-01 13:45:50 +0200

    Some updates in SystemController

--- Citre-app/app/Http/Controllers/systemController.php
+++ Citre-app/app/Http/Controllers/systemController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\newLaravelTips;
 
 use Request;
 
@@ -9,14 +11,13 @@ class systemController extends Controller
     //Inicio dos meus metodos
     public function sendEmail()
     {
-        $data = array(
+        $data = array( 
             $nome=Request::input('nome'),
             $apelido=Request::input('apelido'),
             $email=Request::input('email'),
             $mensagem=Request::input('mensagem'),
         );
 
-        Mail::to(Request::input('email'))->send(new SendMail($data));
-        return new \App\Mail\newLaravelTips();
+        Mail::to(Request::input('email'))->send(new newLaravelTips($data));
     }
 }
