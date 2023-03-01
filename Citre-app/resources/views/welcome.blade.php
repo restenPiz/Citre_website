705bd6c4facd44d0786a3cfa417ae164626c9d10
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-01 16:13:49 +0200

    Update in welcomePage

--- Citre-app/resources/views/welcome.blade.php
+++ Citre-app/resources/views/welcome.blade.php
@@ -254,8 +254,8 @@
 
         <!-- ======= Clients Section ======= -->
         <section id="clients" class="clients section-bg">
-            <div class="container" data-aos="zoom-in">
-
+            <div class="container" data-aos="zoom-in"><br>
+                
                 <div class="row">
 
                     <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
