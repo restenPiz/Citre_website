2aacec7be6a793a87aa77ec1e6d09ea7fad6f830
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-06 10:45:40 +0200

    Some updates in welcomePage

--- Citre-app/resources/views/welcome.blade.php
+++ Citre-app/resources/views/welcome.blade.php
@@ -203,7 +203,7 @@
                         data-aos-delay="100">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bxl-dribbble"></i></div>
-                            <h6><b>Elaboração de estudos</b></h6>
+                            <h6><a href=""><b>Elaboração de estudos</b></a></h6>
                             <p>Somos líderes no desenvolvimento de estudos em três dimensões...</p>
                         </div>
                     </div>
@@ -212,7 +212,7 @@
                         data-aos-delay="200">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bx-file"></i></div>
-                            <h6><b>Elaboração de instrumentos institucionais</b></h6>
+                            <h6><a href=""><b>Elaboração de instrumentos institucionais</b></a></h6>
                             <p>Somos experientes na elaboração de documentos de suporte institucionais...</p>
                         </div>
                     </div>
@@ -221,7 +221,7 @@
                         data-aos-delay="300">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bx-tachometer"></i></div>
-                            <h6><b>Elaboração de projectos de investimento e análise económica</b></h6>
+                            <h6><a href=""><b>Elaboração de projectos de investimento e análise económica</b></a></h6>
                             <p>Elaboramos Projectos de viabilidade económica para investimentos...</p>
                         </div>
                     </div>
@@ -230,7 +230,7 @@
                         data-aos-delay="100">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bx-world"></i></div>
-                            <h6><b>Elaboração de projectos de intervenção social</b></h6>
+                            <h6><a href=""><b>Elaboração de projectos de intervenção social</b></a></h6>
                             <p>Somos especializados na elaboração e acompanhamento na implementação de projectos...</p>
                         </div>
                     </div>
@@ -239,7 +239,7 @@
                         data-aos-delay="200">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bx-slideshow"></i></div>
-                            <h6><b>Monitoria e avaliação de projectos</b></h6>
+                            <h6><a href=""><b>Monitoria e avaliação de projectos</b></a></h6>
                             <p>Realizamos actividades de monitorias de implementação de projectos e negócios através de sistemas...</p>
                         </div>
                     </div>
@@ -248,7 +248,7 @@
                         data-aos-delay="300">
                         <div class="icon-box">
                             <div class="icon"><i class="bx bx-arch"></i></div>
-                            <h6><b>Treinamentos/formação e em diversas áreas sociais</b></h6>
+                            <h6><a href=""><b>Treinamentos/formação e em diversas áreas sociais</b></a></h6>
                             <p>Somos líderes em facilitação de formações, capacitações e de debates através de...</p>
                         </div>
                     </div>
