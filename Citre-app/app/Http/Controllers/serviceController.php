7f549091f607bb75f4a7e1aaf71d8d23e650d41c
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-03-06 11:50:52 +0200

    Some updates in controller

--- Citre-app/app/Http/Controllers/serviceController.php
+++ Citre-app/app/Http/Controllers/serviceController.php
@@ -10,4 +10,24 @@ class serviceController extends Controller
     {
         return view('services.firstServices');
     }
+    public function secondService()
+    {
+        return view('services.secondServices');
+    }
+    public function thirdService()
+    {
+        return view('services.thirdServices');
+    }
+    public function fourthService()
+    {
+        return view('services.fourthServices');
+    }
+    public function fifthService()
+    {
+        return view('services.fifthServices');
+    }
+    public function sixthService()
+    {
+        return view('services.sixthServices');
+    }
 }
